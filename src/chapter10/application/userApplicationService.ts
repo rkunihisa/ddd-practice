215ffdc1973d9820545dce4bc10862bf02840856
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-08-26 07:31:13 +0900

    feat: refactor register method in UserApplicationService to use Transactional decorator for transaction management

--- src/chapter10/application/userApplicationService.ts
+++ src/chapter10/application/userApplicationService.ts
@@ -6,6 +6,24 @@ import { inject, injectable } from "inversify";
 import type { UserFactoryInterface } from "../domain/model/user/userFactoryInterface";
 import * as mysql from "mysql2/promise";
 
+// AOPデコレーター（クラス外で定義）
+function Transactional(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+        // @ts-ignore 実行時にはthisはUserApplicationServiceのインスタンスになる
+        const connection = this.connection;
+        await connection.beginTransaction();
+        try {
+            const result = await originalMethod.apply(this, args);
+            await connection.commit();
+            return result;
+        } catch (err) {
+            await connection.rollback();
+            throw err;
+        }
+    };
+}
+
 @injectable()
 export class UserApplicationService {
     constructor(
@@ -15,19 +33,15 @@ export class UserApplicationService {
         private readonly connection: mysql.Connection
     ) { }
 
+    @Transactional
     public async register(command: UserRegisterCommand): Promise<void> {
-        await this.connection.beginTransaction();
-        try {
-            const userName = new UserName(command.name);
-            const user = this.userFactory.create(userName);
-            if (await this.userService.exists(user)) {
-                throw new Error("ユーザは既に存在しています。");
-            }
-            await this.userRepository.save(user);
-            await this.connection.commit();
-        } catch (err) {
-            await this.connection.rollback();
-            throw err;
+        const userName = new UserName(command.name);
+        const user = this.userFactory.create(userName);
+        if (await this.userService.exists(user)) {
+            throw new Error("ユーザは既に存在しています。");
         }
+        await this.userRepository.save(user);
     }
 }
+
+
